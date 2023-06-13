067adca56b798effe5fac9f5eda42dc963f9a120
Neradovskikh Vladimir <skih.vladimir@gmail.com>
2023-06-13 14:21:33 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,6 @@
+new SimpleBar(
+  document.getElementById('simplebar-example'),
+  { autoHide: false }
+);
+
+const osInstance = OverlayScrollbarsGlobal.OverlayScrollbars(document.getElementById('overlayscrollbars-example'), {});
